904d82395d3c793d375c939e3e5361f2b5f3a9c3
WOONBE <kepull2918@naver.com>
2024-10-28 15:08:40 +0900

    chore - S11P31D205-45 : 프로젝트 생성

---
+++ KIWI-Backend/src/main/java/com/d205/KIWI_Backend/KiwiBackendApplication.java
@@ -0,0 +1,13 @@
+package com.d205.KIWI_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KiwiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KiwiBackendApplication.class, args);
+	}
+
+}
