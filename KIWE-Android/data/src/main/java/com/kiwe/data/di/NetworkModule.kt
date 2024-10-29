6b384ccd24667d6a5ceb2dda84d61cbb2d6e22c2
전형선 <hsun5163@gmail.com>
2024-10-29 09:59:30 +0900

    ✨feat - S11P31D205-96 : NetworkModule 추가

---
+++ KIWE-Android/data/src/main/java/com/kiwe/data/di/NetworkModule.kt
@@ -0,0 +1,78 @@
+package com.kiwe.data.di
+
+import android.util.Log
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.android.Android
+import io.ktor.client.plugins.DefaultRequest
+import io.ktor.client.plugins.HttpTimeout
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.logging.LogLevel
+import io.ktor.client.plugins.logging.Logger
+import io.ktor.client.plugins.logging.Logging
+import io.ktor.client.plugins.observer.ResponseObserver
+import io.ktor.client.request.accept
+import io.ktor.client.request.header
+import io.ktor.http.ContentType
+import io.ktor.http.HttpHeaders
+import io.ktor.http.contentType
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.json.Json
+import javax.inject.Singleton
+
+private const val TAG = "NetworkModule"
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+    private const val BASE_URL = "https://702bb8b6-4cfb-4bd5-8e8c-33553ee83b0c.mock.pstmn.io/" // Postman Mock Server
+    private const val NETWORK_TIMEOUT = 5000L
+
+    @Provides
+    @Singleton
+    fun provideHttpClient() = HttpClient(Android) {
+
+        install(ContentNegotiation) {
+            json(
+                Json {
+                    prettyPrint = true
+                    isLenient = true
+                    useAlternativeNames = true
+                    ignoreUnknownKeys = true
+                    encodeDefaults = false
+                }
+            )
+        }
+
+        install(HttpTimeout) {
+            requestTimeoutMillis = NETWORK_TIMEOUT
+            connectTimeoutMillis = NETWORK_TIMEOUT
+            socketTimeoutMillis = NETWORK_TIMEOUT
+        }
+
+        install(Logging) {
+            logger = object: Logger {
+                override fun log(message: String) {
+                    Log.v(TAG, message)
+                }
+            }
+            level = LogLevel.ALL
+        }
+
+        install(ResponseObserver) {
+            onResponse { response ->
+                Log.d(TAG, "HTTP status: ${response.status.value}")
+
+            }
+        }
+
+        install(DefaultRequest) {
+            header(HttpHeaders.ContentType, ContentType.Application.Json)
+            contentType(ContentType.Application.Json)
+            accept(ContentType.Application.Json)
+            host = BASE_URL
+        }
+    }
+}
