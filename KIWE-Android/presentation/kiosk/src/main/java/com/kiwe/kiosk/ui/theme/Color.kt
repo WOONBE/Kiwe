c427ef881e012ada95bc147840503890ee8b4750
전형선 <hsun5163@gmail.com>
2024-11-04 13:36:20 +0900

    💄style - S11P31D205-201 : Kiwe 색상 추가

--- KIWE-Android/presentation/kiosk/src/main/java/com/kiwe/kiosk/ui/theme/Color.kt
+++ KIWE-Android/presentation/kiosk/src/main/java/com/kiwe/kiosk/ui/theme/Color.kt
@@ -23,3 +23,19 @@ val KioskBackgroundBrush =
         startY = 0f,
         endY = Float.POSITIVE_INFINITY,
     )
+
+val KiweWhite1 = Color(0xFFFFFFFF)
+val KiweBlack1 = Color(0xFF000000)
+val KiweSilver1 = Color(0xFFD6D6D6)
+val KiweGray1 = Color(0xFF6D6D6D)
+val KiweBrown1 = Color(0xFFEDE0D4)
+val KiweBrown2 = Color(0xFFD2B48C)
+val KiweBrown3 = Color(0xFFA67B5B)
+val KiweBrown4 = Color(0xFF7B4F3F)
+val KiweBrown5 = Color(0xFF4E342E)
+val KiweGreen1 = Color(0xFFE8F5E9)
+val KiweGreen2 = Color(0xFFA8D5BA)
+val KiweGreen3 = Color(0xFF81C784)
+val KiweGreen4 = Color(0xFF66BB6A)
+val KiweGreen5 = Color(0xFF2E7D32)
+val KiweOrange1 = Color(0xFFE14700)
