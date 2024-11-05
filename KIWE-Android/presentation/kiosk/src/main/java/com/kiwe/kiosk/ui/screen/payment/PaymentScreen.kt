233b74795023253533d22bd80b6e199a7d46a2e0
전형선 <hsun5163@gmail.com>
2024-11-05 14:54:30 +0900

    💄style - S11P31D205-201 : 결제 프로세스 화면 UI 작성

--- KIWE-Android/presentation/kiosk/src/main/java/com/kiwe/kiosk/ui/screen/payment/PaymentScreen.kt
+++ KIWE-Android/presentation/kiosk/src/main/java/com/kiwe/kiosk/ui/screen/payment/PaymentScreen.kt
@@ -3,11 +3,13 @@ package com.kiwe.kiosk.ui.screen.payment
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.lifecycle.viewmodel.compose.viewModel
 import com.kiwe.kiosk.main.MainViewModel
-import com.kiwe.kiosk.ui.screen.payment.component.ChoiceScreen
 
 enum class PaymentStatus(
     val page: Int,
@@ -23,6 +25,8 @@ fun PaymentScreen(
     viewModel: MainViewModel = hiltViewModel(),
     onBackClick: () -> Unit = {},
 ) {
+    var showDialog by remember { mutableStateOf(false) }
+
     val pagerState =
         rememberPagerState(pageCount = {
             PaymentStatus.entries.size
@@ -33,19 +37,27 @@ fun PaymentScreen(
         state = pagerState,
         userScrollEnabled = true, // TODO : 테스트 후 false
     ) { page ->
-        when(PaymentStatus.entries[page]) {
+        when (PaymentStatus.entries[page]) {
             PaymentStatus.TAKEOUT -> {
-                ChoiceScreen(
+                TakeOutChoiceScreen(
                     modifier = Modifier,
+                    onClick = { showDialog = true },
                 )
+                if (showDialog) {
+                    CardCreditDialog(
+                        onDismissRequest = { showDialog = false },
+                    )
+                }
             }
+
             PaymentStatus.POINT -> {
-                ChoiceScreen(
+                TakeOutChoiceScreen(
                     modifier = Modifier,
                 )
             }
+
             PaymentStatus.CARD -> {
-                ChoiceScreen(
+                TakeOutChoiceScreen(
                     modifier = Modifier,
                 )
             }
--- KIWE-Android/presentation/kiosk/src/main/java/com/kiwe/kiosk/ui/screen/payment/TakeOutChoiceScreen.kt
+++ KIWE-Android/presentation/kiosk/src/main/java/com/kiwe/kiosk/ui/screen/payment/TakeOutChoiceScreen.kt
@@ -1,9 +1,7 @@
-package com.kiwe.kiosk.ui.screen.payment.component
+package com.kiwe.kiosk.ui.screen.payment
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -11,36 +9,28 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.Icon
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.shadow
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.kiwe.kiosk.R
 import com.kiwe.kiosk.ui.component.BoldTextWithKeywords
+import com.kiwe.kiosk.ui.screen.payment.component.ChoiceButton
 import com.kiwe.kiosk.ui.theme.KIWEAndroidTheme
 import com.kiwe.kiosk.ui.theme.KioskBackgroundBrush
 import com.kiwe.kiosk.ui.theme.KiweGray1
 import com.kiwe.kiosk.ui.theme.KiweGreen5
 import com.kiwe.kiosk.ui.theme.KiweOrange1
-import com.kiwe.kiosk.ui.theme.KiweWhite1
 import com.kiwe.kiosk.ui.theme.Typography
 
 @Composable
-fun ChoiceScreen(modifier: Modifier = Modifier) {
+fun TakeOutChoiceScreen(
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit = {},
+) {
     Column(
         modifier =
             modifier
@@ -67,54 +57,16 @@ fun ChoiceScreen(modifier: Modifier = Modifier) {
             horizontalArrangement = Arrangement.SpaceEvenly,
         ) {
             ChoiceButton(
+                iconResourceId = R.drawable.ic_payment_packaging,
                 label = "포장",
                 backgroundColor = KiweGray1,
-                iconResourceId = R.drawable.ic_payment_packaging, // 포장 아이콘 리소스 ID 변경 필요
+                onClick = onClick,
             )
             ChoiceButton(
+                iconResourceId = R.drawable.ic_payment_shop,
                 label = "매장",
                 backgroundColor = KiweGreen5,
-                iconResourceId = R.drawable.ic_payment_shop, // 매장 아이콘 리소스 ID 변경 필요
-            )
-        }
-    }
-}
-
-@Composable
-fun ChoiceButton(
-    modifier: Modifier = Modifier,
-    label: String,
-    backgroundColor: Color,
-    iconResourceId: Int,
-) {
-    Card(
-        modifier = modifier
-            .size(150.dp)
-            .clickable { /* 클릭 이벤트 처리 */ },
-        shape = RoundedCornerShape(16.dp),
-        elevation = CardDefaults.cardElevation(4.dp), // elevation 설정
-        colors = CardDefaults.cardColors(containerColor = backgroundColor) // 백그라운드 색상 설정
-    ) {
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(16.dp),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally,
-        ) {
-            Icon(
-                painter = painterResource(id = iconResourceId),
-                contentDescription = null,
-                tint = Color.White,
-                modifier = Modifier.size(64.dp),
-            )
-            Spacer(modifier = Modifier.height(8.dp))
-
-            Text(
-                modifier = Modifier,
-                text = label,
-                color = KiweWhite1,
-                style = Typography.bodyMedium.copy(fontSize = 32.sp)
+                onClick = onClick,
             )
         }
     }
@@ -122,8 +74,8 @@ fun ChoiceButton(
 
 @Preview
 @Composable
-fun ChoiceScreenPreview() {
+private fun Preview() {
     KIWEAndroidTheme {
-        ChoiceScreen(modifier = Modifier.background(brush = KioskBackgroundBrush))
+        TakeOutChoiceScreen(modifier = Modifier.background(brush = KioskBackgroundBrush))
     }
 }
