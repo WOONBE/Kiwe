1e87a5f5eafa95c71fb5adeb9acc230d50236b5a
이문현 <hogandi001@gmail.com>
2024-11-06 20:57:30 +0900

    🚑fix : merge conflict 수정 (import error)

--- KIWI-Backend/src/main/java/com/d205/KIWI_Backend/order/repository/OrderRepository.java
+++ KIWI-Backend/src/main/java/com/d205/KIWI_Backend/order/repository/OrderRepository.java
@@ -2,16 +2,12 @@ package com.d205.KIWI_Backend.order.repository;
 
 import com.d205.KIWI_Backend.order.domain.Order;
 import io.lettuce.core.dynamic.annotation.Param;
-import java.util.List;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
-    @Modifying
     @Transactional
     @Query(value = "SELECT status FROM orders WHERE kiosk_id = :kioskId ORDER BY order_date DESC LIMIT 1", nativeQuery = true)
     String findLatestStatusByKioskId(@Param("kioskId") Long kioskId);
