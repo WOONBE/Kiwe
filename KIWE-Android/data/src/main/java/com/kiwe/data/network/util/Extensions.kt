f2b5d112533222413e840ae76611eb52871a5eaa
Hanguelmansae <111sym@naver.com>
2024-11-07 16:17:41 +0900

    ✨feat - S11P31D205-242 : 메소드별 result 확장 함수 추가
    S11P31D205-242 #done

--- KIWE-Android/data/src/main/java/com/kiwe/data/network/util/Extensions.kt
+++ KIWE-Android/data/src/main/java/com/kiwe/data/network/util/Extensions.kt
@@ -5,7 +5,11 @@ import com.kiwe.domain.exception.ErrorResponse
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.request.HttpRequestBuilder
+import io.ktor.client.request.delete
 import io.ktor.client.request.get
+import io.ktor.client.request.patch
+import io.ktor.client.request.post
+import io.ktor.client.request.put
 import io.ktor.client.statement.bodyAsText
 import io.ktor.http.isSuccess
 import kotlinx.io.IOException
@@ -17,6 +21,122 @@ suspend inline fun <reified T> HttpClient.getResult(
 ): Result<T> =
     try {
         val response = get(urlString = url, block = httpRequestBuilder)
+        if (response.status.isSuccess()) {
+            Result.success(decodeFromString(response.body()))
+        } else {
+            val errorBody = response.bodyAsText()
+            val errorResponse = decodeFromString(ErrorResponse.serializer(), errorBody)
+            Result.failure(
+                APIException(
+                    message = errorResponse.message,
+                    code = errorResponse.code,
+                    throwable = IOException("API error"),
+                ),
+            )
+        }
+    } catch (e: Exception) {
+        Result.failure(
+            APIException(
+                message = e.message ?: "에러 메세지가 없습니다!",
+                code = 1,
+                throwable = e,
+            ),
+        )
+    }
+
+suspend inline fun <reified T> HttpClient.postResult(
+    url: String,
+    httpRequestBuilder: HttpRequestBuilder.() -> Unit = {},
+): Result<T> =
+    try {
+        val response = post(urlString = url, block = httpRequestBuilder)
+        if (response.status.isSuccess()) {
+            Result.success(decodeFromString(response.body()))
+        } else {
+            val errorBody = response.bodyAsText()
+            val errorResponse = decodeFromString(ErrorResponse.serializer(), errorBody)
+            Result.failure(
+                APIException(
+                    message = errorResponse.message,
+                    code = errorResponse.code,
+                    throwable = IOException("API error"),
+                ),
+            )
+        }
+    } catch (e: Exception) {
+        Result.failure(
+            APIException(
+                message = e.message ?: "에러 메세지가 없습니다!",
+                code = 1,
+                throwable = e,
+            ),
+        )
+    }
+
+suspend inline fun <reified T> HttpClient.deleteResult(
+    url: String,
+    httpRequestBuilder: HttpRequestBuilder.() -> Unit = {},
+): Result<T> =
+    try {
+        val response = delete(urlString = url, block = httpRequestBuilder)
+        if (response.status.isSuccess()) {
+            Result.success(decodeFromString(response.body()))
+        } else {
+            val errorBody = response.bodyAsText()
+            val errorResponse = decodeFromString(ErrorResponse.serializer(), errorBody)
+            Result.failure(
+                APIException(
+                    message = errorResponse.message,
+                    code = errorResponse.code,
+                    throwable = IOException("API error"),
+                ),
+            )
+        }
+    } catch (e: Exception) {
+        Result.failure(
+            APIException(
+                message = e.message ?: "에러 메세지가 없습니다!",
+                code = 1,
+                throwable = e,
+            ),
+        )
+    }
+
+suspend inline fun <reified T> HttpClient.patchResult(
+    url: String,
+    httpRequestBuilder: HttpRequestBuilder.() -> Unit = {},
+): Result<T> =
+    try {
+        val response = patch(urlString = url, block = httpRequestBuilder)
+        if (response.status.isSuccess()) {
+            Result.success(decodeFromString(response.body()))
+        } else {
+            val errorBody = response.bodyAsText()
+            val errorResponse = decodeFromString(ErrorResponse.serializer(), errorBody)
+            Result.failure(
+                APIException(
+                    message = errorResponse.message,
+                    code = errorResponse.code,
+                    throwable = IOException("API error"),
+                ),
+            )
+        }
+    } catch (e: Exception) {
+        Result.failure(
+            APIException(
+                message = e.message ?: "에러 메세지가 없습니다!",
+                code = 1,
+                throwable = e,
+            ),
+        )
+    }
+
+suspend inline fun <reified T> HttpClient.putResult(
+    url: String,
+    httpRequestBuilder: HttpRequestBuilder.() -> Unit = {},
+): Result<T> =
+    try {
+        val response = put(urlString = url, block = httpRequestBuilder)
         if (response.status.isSuccess()) {
             Result.success(decodeFromString(response.body()))
         } else {
