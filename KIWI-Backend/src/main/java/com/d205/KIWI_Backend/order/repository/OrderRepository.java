066b14a3e3975971c8e8b354086042515763285b
WOONBE <kepull2918@naver.com>
2024-11-07 10:29:49 +0900

    ✨feat - S11P31D205-168 : 판매량 로그 분석 api 작성

--- KIWI-Backend/src/main/java/com/d205/KIWI_Backend/order/repository/OrderRepository.java
+++ KIWI-Backend/src/main/java/com/d205/KIWI_Backend/order/repository/OrderRepository.java
@@ -2,6 +2,8 @@ package com.d205.KIWI_Backend.order.repository;
 
 import com.d205.KIWI_Backend.order.domain.Order;
 import io.lettuce.core.dynamic.annotation.Param;
+import java.time.LocalDateTime;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -16,5 +18,13 @@ public interface OrderRepository extends JpaRepository<Order, Long> {
     @Transactional
     @Query(value = "UPDATE orders o JOIN (SELECT id FROM orders WHERE kiosk_id = :kioskId ORDER BY order_date DESC LIMIT 1) latest_order ON o.id = latest_order.id SET o.status = 'COMPLETED'", nativeQuery = true)
     int updateOrderStatusToCompleted(@Param("kioskId") Long kioskId);
+
+    @Query("SELECT o FROM Order o JOIN o.kioskOrders ko WHERE ko.kiosk.id = :kioskId AND o.orderDate > :oneMonthAgo")
+    List<Order> findByKioskIdAndOrderDateAfter(@Param("kioskId") Integer kioskId, @Param("oneMonthAgo") LocalDateTime oneMonthAgo);
+
+    @Query("SELECT o FROM Order o JOIN o.kioskOrders ko WHERE ko.kiosk.id = :kioskId AND o.orderDate BETWEEN :startOfMonth AND :endOfMonth")
+    List<Order> findByKioskIdAndOrderDateBetween(@Param("kioskId") Integer kioskId, @Param("startOfMonth") LocalDateTime startOfMonth, @Param("endOfMonth") LocalDateTime endOfMonth);
+
+    List<Order> findByOrderDateAfter(LocalDateTime date);
     
 }
