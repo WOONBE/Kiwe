32de70eff42723c73c9b2ea414460b860d2629eb
Mingyu Kim <mingyu5675@gmail.com>
2024-11-08 15:30:42 +0900

    🚑fix - S11P31D205-252 : usecase 하나 잘못된 거 연결

--- KIWE-Android/data/src/main/java/com/kiwe/data/usecase/manager/kiosk/GetAllMyKioskUseCaseImpl.kt
+++ KIWE-Android/data/src/main/java/com/kiwe/data/usecase/manager/kiosk/GetAllMyKioskUseCaseImpl.kt
@@ -2,13 +2,13 @@ package com.kiwe.data.usecase.manager.kiosk
 
 import com.kiwe.data.network.service.KioskManagerService
 import com.kiwe.domain.model.Kiosk
-import com.kiwe.domain.usecase.manager.kiosk.GetAllKioskUseCase
+import com.kiwe.domain.usecase.manager.kiosk.GetAllMyKioskUseCase
 import javax.inject.Inject
 
 class GetAllMyKioskUseCaseImpl
     @Inject
     constructor(
         private val kioskService: KioskManagerService,
-    ) : GetAllKioskUseCase {
+    ) : GetAllMyKioskUseCase {
         override suspend fun invoke(): List<Kiosk> = kioskService.getMyKiosk().getOrThrow()
     }
