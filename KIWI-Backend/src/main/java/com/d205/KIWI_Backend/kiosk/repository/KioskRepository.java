109e7b40279a8f5cef2df9369a0500bdf0bc4f6c
김헌우 <kepull2918@naver.com>
2024-11-13 17:20:52 +0900

    ✨feat - S11P31D205-262 : 로그인 된 멤버의 수익 및 주문횟수 api 구현

--- KIWI-Backend/src/main/java/com/d205/KIWI_Backend/kiosk/repository/KioskRepository.java
+++ KIWI-Backend/src/main/java/com/d205/KIWI_Backend/kiosk/repository/KioskRepository.java
@@ -13,7 +13,8 @@ import org.springframework.stereotype.Repository;
 public interface KioskRepository extends JpaRepository<Kiosk, Integer> {
 
     List<Kiosk> findByMember(Member member);
-    // 필요한 쿼리 메서드를 추가할 수 있습니다.
+
+    List<Kiosk> findByMemberId(Integer memberId);
 
     @Query(value = "SELECT k.kiosk_order_num " +
             "FROM (SELECT kiosk_id, ROW_NUMBER() OVER(ORDER BY kiosk_id) AS kiosk_order_num " +
@@ -22,4 +23,6 @@ public interface KioskRepository extends JpaRepository<Kiosk, Integer> {
             "WHERE k.kiosk_id = :kioskId", nativeQuery = true)
     Long findKioskOrderNumberByOwnerIdAndKioskId(@Param("ownerId") Long ownerId,
                                                     @Param("kioskId") Long kioskId);
+
+
 }
