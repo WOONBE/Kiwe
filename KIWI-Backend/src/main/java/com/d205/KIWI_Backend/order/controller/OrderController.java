9368a45ea488db9296c97418a523288023e5bcd9
김헌우 <kepull2918@naver.com>
2024-11-15 13:04:53 +0900

    ✨feat - S11P31D205-279 : 특정 멤버가 운영하는 키오스크의 월별 매출 api 구현

--- KIWI-Backend/src/main/java/com/d205/KIWI_Backend/order/controller/OrderController.java
+++ KIWI-Backend/src/main/java/com/d205/KIWI_Backend/order/controller/OrderController.java
@@ -1,6 +1,7 @@
 package com.d205.KIWI_Backend.order.controller;
 
 import com.d205.KIWI_Backend.global.exception.BadRequestException;
+import com.d205.KIWI_Backend.member.service.MemberService;
 import com.d205.KIWI_Backend.menu.dto.MenuResponse;
 import com.d205.KIWI_Backend.order.dto.MenuSales;
 import com.d205.KIWI_Backend.order.dto.OrderRequest;
@@ -20,10 +21,12 @@ import org.springframework.web.bind.annotation.*;
 public class OrderController {
 
     private final OrderService orderService;
+    private final MemberService memberService;
 
     @Autowired
-    public OrderController(OrderService orderService) {
+    public OrderController(OrderService orderService, MemberService memberService) {
         this.orderService = orderService;
+        this.memberService = memberService;
     }
 
     @PostMapping
@@ -160,13 +163,13 @@ public class OrderController {
     }
 
 
-//    @GetMapping("/order-count-last-month")
-//    @Operation(summary = "로그인 된 멤버가 운영하는 키오스크의 총 주문 횟수", description = "로그인 된 멤버가 운영하는 키오스크의 총 주문 횟수을 리턴하는 API")
-//    public ResponseEntity<Integer> getOrderCountForLastMonth() {
-//        int orderCount = orderService.calculateOrderCountForLastMonthByMemberId();
-//
-//        return ResponseEntity.ok(orderCount);
-//    }
+    @GetMapping("/order-count-last-month")
+    @Operation(summary = "로그인 된 멤버가 운영하는 키오스크의 총 주문 횟수", description = "로그인 된 멤버가 운영하는 키오스크의 총 주문 횟수을 리턴하는 API")
+    public ResponseEntity<Integer> getOrderCountForLastMonth() {
+        int orderCount = orderService.calculateOrderCountForLastMonthByMemberId();
+
+        return ResponseEntity.ok(orderCount);
+    }
 
     @GetMapping("/top-20-menu-sales")
     @Operation(summary = "로그인 된 멤버가 운영하는 키오스크의 판매량 별 메뉴 조회", description = "로그인 된 멤버가 운영하는 키오스크의 판매량 별 메뉴 조회하는 API")
@@ -174,12 +177,15 @@ public class OrderController {
         // 서비스 메서드를 호출하여 판매 수량이 많은 상위 20개의 메뉴 가져오기
         List<MenuResponse> top20MenuSales = orderService.getTop20MenuSalesForLastMonthByMemberId();
 
-        // 결과 반환
-        if (top20MenuSales.isEmpty()) {
-            return ResponseEntity.noContent().build();
-        }
-
         return ResponseEntity.ok(top20MenuSales);
+    }
+
+    @GetMapping("/monthly-sales/last-six-months/member")
+    @Operation(summary = "멤버의 키오스크 월별 매출 조회", description = "특정 멤버가 운영하는 키오스크의 지난 6개월 동안의 매출을 월별로 리턴하는 API")
+    public ResponseEntity<Map<YearMonth, Integer>> getMonthlySalesForLastSixMonthsByMemberId() {
+        Integer memberId = memberService.getCurrentMemberId();
+        Map<YearMonth, Integer> monthlySales = orderService.getMonthlySalesForLastSixMonthsByMemberId(memberId);
+        return ResponseEntity.ok(monthlySales);
     }
 
 
